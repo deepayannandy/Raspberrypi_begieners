f4fc41b138e663af80ea296d0119e8f0517c4475
DEEPAYAN NANDY <deepayan.622@gmail.com>
2020-04-24 16:41:13 +0530

    Update Simple_motor.py

--- Chapter2/Motor_driver/Simple_motor.py
+++ Chapter2/Motor_driver/Simple_motor.py
@@ -3,32 +3,24 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-m1a,m1b=8,7
+m1a,m1b=8,7                     # defining motor pins
 
-GPIO.setup(m1a,GPIO.OUT)
-GPIO.setup(m1b,GPIO.OUT)
+GPIO.setup(m1a,GPIO.OUT)        # set the motor pins as output
+GPIO.setup(m1b,GPIO.OUT)        # set the motor pins as output
 
-
-
-def forward():
+def forward():                  # rotate the motor clock wise
     GPIO.output(m1a,True)
     GPIO.output(m1b,False)
 
-
-
 def backword():
-    GPIO.output(m1a,False)
+    GPIO.output(m1a,False)      # rotate the motor anti clock wise
     GPIO.output(m1b,True)
 
-
-    
 def stop():
-    GPIO.output(m1a,False)
+    GPIO.output(m1a,False)      # Stops the motor
     GPIO.output(m1b,False)
-
-
-
-while True:
+    
+while True:     
     forward()
     time.sleep(1)
     stop()
